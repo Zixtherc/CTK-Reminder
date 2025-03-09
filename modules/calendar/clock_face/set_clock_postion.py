11de17a44c86635fdb483a9d8a324926c8421fb2
Zixther <duckandfiretto@gmail.com>
2025-03-09 14:30:43 +0200

    Developed a function that centers buttons using radians

--- modules/calendar/clock_face/set_clock_postion.py
+++ modules/calendar/clock_face/set_clock_postion.py
@@ -4,5 +4,19 @@ import customtkinter as ctk
 # Обязательный импорт для вычесления размещения
 import math
 
-def circle_position(radius: int = 150):
-    pass
+root = ctk.CTk()
+root.geometry("400x400")
+
+def circle_position(radius: int = 150,y_axis: int = 200, x_axis: int = 200):
+    result_position = []
+    # Перебираем по 12 раз, потому что в циферблате 12 часов
+    for clock_face in range(12):
+        # Вычисляем координаты точки на круге, множим на 30, потому что 12*30 = 360. 360 Это градусная мера круга
+        angle = math.radians(clock_face * 30)
+        # Вычисляем координаты, множим на косинус, потому что в декартовых координатах, в программировании, косинус управляет осью X
+        x_cor = x_axis + radius * math.cos(angle)
+        # Вычисляем координаты, множим на cинус, потому что в декартовых координатах, в программировании, cинус управляет осью Y
+        y_cor = y_axis + radius * math.sin(angle)
+        result_position.append((x_cor, y_cor))
+    return result_position
+
