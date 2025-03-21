7037bba91ff935b9dbff7d357f45be1862309d65
Zixther <duckandfiretto@gmail.com>
2025-03-22 00:03:25 +0200

    Animation func completed

--- modules/window_funcs/animation.py
+++ modules/window_funcs/animation.py
@@ -1,9 +1,10 @@
 import customtkinter as ctk
 
-def update_animation_steps():
-    pass
+def update_animation_steps(step: int, steps: int, animated_obj: object, step_size: int, canvas = ctk.CTkCanvas):
+    if step < steps:
+        canvas.move(animated_obj, step_size, 0)
 
-def animation(shift_distance: int, steps: int = 10):
+def animation(shift_distance: int, animated_obj: object, canvas: ctk.CTkCanvas, steps: int = 10):
     '''
     #### `Функция`, которая `выполняет` анимацию сдвига ####
 
@@ -13,6 +14,10 @@ def animation(shift_distance: int, steps: int = 10):
 
     Пример использования:
     ```python 
-    ПОКА НЕ ДОДЕЛАЛ
+    ПОКА НЕ ДОДЕЛАЛ 
     ''' 
-    
+    # Расчитываем сколько будет занимать один шаг анимации
+    step_size = shift_distance // steps
+    # Обнуляем общее количество шагов
+    step = 0
+    update_animation_steps(step = step, steps = steps, animated_obj = animated_obj, step_size = step_size, canvas = canvas) 
